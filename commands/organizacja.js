ee21e3d8378b7dfd71ef30917c30d4aa4609b674
Mleczuuu <mleczutokozak@gmail.com>
2022-07-22 18:01:46 +0200

    wiecej informacji pod /organizacja

--- commands/organizacja.js
+++ commands/organizacja.js
@@ -11,13 +11,15 @@ module.exports = {
         if (!gid.paid) return interaction.reply({ embeds: [bot.prettyReply("**Hola hola, bot dla tej organizacji nie jest opłacony.**", interaction)], ephemeral: true })
         const fields = [
             { name: "**Konto bota**", value: `${gid.settings.client.username}`, inline: true },
-            { name: "**Haslo bota**", value: `${((interaction.user.id == config.discord.ownerId) ? gid.settings.client.password : "Ukryte")}`, inline: false },
+            { name: "**Haslo bota**", value: `${((interaction.user.id == config.discord.ownerId) ? gid.settings.client.password : "Ukryte")}`, inline: true },
             { name: "**ID organizacji**", value: `${gid.id}`, inline: true },
-            { name: "**Opłacona do**", value: new Date(gid.paidTo).toLocaleDateString('pl-PL', { hour: '2-digit', minute: '2-digit', year: 'numeric', month: 'long', day: 'numeric' }), inline: false},
+            { name: "**Opłacona do**", value: new Date(gid.paidTo).toLocaleDateString('pl-PL', { hour: '2-digit', minute: '2-digit', year: 'numeric', month: 'long', day: 'numeric' }), inline: true},
             { name: "**Kanał: Importy**", value: `<#${gid.settings.discord.channels.import_success}>`, inline: true },
-            { name: "**Kanał: Exporty**", value: `<#${gid.settings.discord.channels.export}>`, inline: false },
+            { name: "**Kanał: Nieudane importy**", value: `<#${gid.settings.discord.channels.import_fail}>`, inline: true },
+            { name: "**Kanał: Exporty**", value: `<#${gid.settings.discord.channels.export}>`, inline: true },
+            { name: "**Kanał: Rozpoczęcie artefaktu**", value: `<#${gid.settings.discord.channels.artifact_start}>`, inline: true },
             { name: "**Kanał: Artefakty**", value: `<#${gid.settings.discord.channels.artifact_end}>`, inline: true },
-            { name: "**Kanał: Lombard**", value: `<#${gid.settings.discord.channels.pawnshop}>`, inline: false },
+            { name: "**Kanał: Lombard**", value: `<#${gid.settings.discord.channels.pawnshop}>`, inline: true },
             
         ]
         return interaction.reply({ embeds: [bot.prettyReply("**Informajce o organizacji**", interaction).addFields(fields)], ephemeral: true })
