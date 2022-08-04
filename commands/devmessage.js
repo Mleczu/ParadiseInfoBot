95070b53a7bb807f962ab411712dad3582cb18bf
Mleczuuu <mleczutokozak@gmail.com>
2022-08-04 02:40:48 +0200

    required dev message

--- commands/devmessage.js
+++ commands/devmessage.js
@@ -9,6 +9,7 @@ module.exports = {
             return o
                 .setName("wiadomosc")
                 .setDescription("Wiadomość")
+                .setRequired(true)
         }),
 	async execute(bot, interaction) {
         if (interaction.user.id != config.discord.ownerId) return interaction.reply({ embeds: [bot.prettyReply("**Hola hola, nie masz do tego uprawnień.**", interaction)], ephemeral: true })
