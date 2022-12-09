19636071eef4f9841a4c1a554c0a9a0b408d8224
Mleczuuu <mleczutokozak@gmail.com>
2022-12-10 00:35:29 +0100

    ssa

--- bot.js
+++ bot.js
@@ -64,7 +64,7 @@ class Instance {
         this.createCronJob('30 0 * * * *', this.Ping3DaysLeftWarehouse);
         this.createCronJob('1 * * * * *', this.ProcessQueue);
         this.createCronJob('0 59 23 * * *', this.GenerateDailyReport);
-        this.createCronJob('0 33 * * * *', this.GeneratePayoutPreview);
+        this.createCronJob('0 37 * * * *', this.GeneratePayoutPreview);
         this.createCronJob('*/10 * * * * *', this.SendQueueList);
         return this
     }
